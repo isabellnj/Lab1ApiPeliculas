9979c2d428ad06beb8ff041d7b9c48e86ce61e89
DESKTOP-3M2FJBS\USUARIO <isabellanaja0@gmail.com>
2020-11-04 17:56:19 +0100

    commit de creacion

---
+++ src/main/java/com/example/Lab1ApiPeliculas/Lab1ApiPeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab1ApiPeliculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1ApiPeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1ApiPeliculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Lab1ApiPeliculas/Lab1ApiPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab1ApiPeliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ApiPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
