b5d03626fe3ca5f6116bbebee1ce825098cc98a8
DESKTOP-3M2FJBS\USUARIO <isabellanaja0@gmail.com>
2020-11-07 13:00:15 +0100

    Ejercicio7

--- src/main/java/com/example/Lab1ApiPeliculas/Api/ControladorPeliculas.java
+++ src/main/java/com/example/Lab1ApiPeliculas/Api/ControladorPeliculas.java
@@ -180,6 +180,12 @@ public class ControladorPeliculas {
    }
 
 
+
+   
+    
+
+
+   
 }
 
 
---
+++ src/main/java/com/example/Lab1ApiPeliculas/Api/ControllerTraductor.java
@@ -0,0 +1,27 @@
+package com.example.Lab1ApiPeliculas.Api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerTraductor {
+
+
+    //Ejericicio7-->idioma en html
+
+    @GetMapping("/testhtml")
+    public String Test(){
+        
+        return "test";
+
+    }
+
+
+    @GetMapping("/testhtml1")
+    public String Test1(){
+        
+        return "test2";
+
+    }
+    
+}
